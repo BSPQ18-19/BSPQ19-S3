ad375aaa0748a2318deba409c8d5948078cd9973
josu <josu>
2019-04-01 21:12:26 +0200

    Borrado IRmi.java innecesario
    No hace falta, ya que la clase existe en el poyecto común que importa el
    servidor

--- ServidorHBN/src/main/java/es/deusto/spq/remote/IRmi.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.spq.remote;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IRmi extends Remote {
-	
-	public boolean login(String usuario, String contrasenya) throws RemoteException;
-}
