a497d7c8f7431f8dbc0a33342b9153b6999eb805
josu <josu>
2019-04-02 16:25:51 +0200

    Es necesario añadir (de momento) la interfaz en el propio servidor para que el RMI lo detecte

---
+++ ServidorHBN/src/main/java/es/deusto/spq/remote/IRmi.java
@@ -0,0 +1,11 @@
+package es.deusto.spq.remote;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IRmi extends Remote {
+	
+	public boolean login(String usuario, String contrasenya) throws RemoteException;
+	
+	public String[] getPerfiles(String usuario) throws RemoteException;
+}
