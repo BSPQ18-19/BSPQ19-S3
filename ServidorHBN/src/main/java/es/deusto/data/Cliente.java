4bc1ca6770ea4952f8d4577ee32573e6c72f2627
Iker <ikerchavez@opendeusto.es>
2019-04-04 18:06:19 +0200

    JDO funcional.

--- ServidorHBN/src/main/java/es/deusto/data/Cliente.java
+++ ServidorHBN/src/main/java/es/deusto/data/Cliente.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.jdo.annotations.*;
 
 @PersistenceCapable
-public class Usuario implements Serializable{
+public class Cliente implements Serializable{
 
 	/**
 	 * 
@@ -18,14 +18,16 @@ public class Usuario implements Serializable{
 	private String pass;
 	private String nick;
 	private int edad;
+	private int tipo;
 	
 	
-	public Usuario(int idUs, String nombre, String pass, String nick, int edad) {
+	public Cliente(int idUs, String nombre, String pass, String nick, int edad, int tipo) {
 		this.idUs = idUs;
 		this.nombre = nombre;
 		this.pass = pass;
 		this.nick = nick;
 		this.edad = edad;
+		this.tipo = tipo;
 	}
 	
 	public int getIdUs() {
@@ -58,10 +60,11 @@ public class Usuario implements Serializable{
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	
+	public int getTipo() {
+		return tipo;
 	}
-	
-	
-	
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
 }
