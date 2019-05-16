ef9f2182eed32f90b75ec432f19ecf8c8b03b9d8
DanielDiazA <33034513+DanielDiazA@users.noreply.github.com>
2019-05-16 20:46:17 +0200

    Jpelicula

--- src/main/java/es/deusto/spq/gui/JValorar.java
+++ src/main/java/es/deusto/spq/gui/JValorar.java
@@ -15,17 +15,18 @@ public class JValorar extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	public int valoracion;
+	public boolean valorado=false;
 	public JValorar(){
 		setLayout(new GridLayout(1, 0, 0, 0));
 		
 		JButton btnNewButton = new JButton("Valorar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
 				String[] options = {"0", "1", "2", "3","4","5"};
 				int seleccion = JOptionPane.showOptionDialog(null, "¿Qué nota le pondrías a esta película?", "Valoración ", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 				System.out.println(seleccion);
 				valoracion=seleccion;
+				valorado=true;
 			}
 		});
 		add(btnNewButton);
